ba0df0bb302ce25a3d2ae5abda9123cb928c4ebf
Ann Kilzer <akilzer@gmail.com>
2018-08-13 15:00:28 +0900

    demo failing solidity test

--- test/TestMikancoin.sol
+++ test/TestMikancoin.sol
@@ -29,4 +29,14 @@ contract TestMikancoin {
     Assert.equal(mikan.balanceOf(fox), tokens, "Fox balance after transfer");
     Assert.equal(mikan.balanceOf(this), startTokens - tokens, "Sender balance after transfer");
   }
+
+  /** This creates a horrible stack trace. Better to test this case in javascript.
+  function testBadTransfer() public {
+    uint startTokens = 100;
+    MikanFarm farm = MikanFarm(DeployedAddresses.MikanFarm());  // Get the address and cast it
+    Mikancoin mikan = farm.deployMikancoin(startTokens);
+    
+    Assert.isFalse(mikan.transfer(0x0, 5), "Transfer to 0 address should fail"); // require() failes and therefore this will revert
+  }
+  */
 }
